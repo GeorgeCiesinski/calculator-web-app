1b58dbb4147fc1f4b1c51d24dda13c41f30b2356
George Ciesinski <george.ciesin@gmail.com>
2022-10-06 17:18:23 -0400

    Numbers display on screen - Added functionality to add numbers and operstors to display - Operators broke when another bug fixed - Numbers add to array item instead of as new items

---
+++ javascript/buttonValues.js
@@ -0,0 +1,64 @@
+const buttonValues = [
+    // Numpad
+    {
+        "id": "key-0",
+        "value": 0
+    },
+    {
+        "id": "key-1",
+        "value": 1
+    },
+    {
+        "id": "key-2",
+        "value": 2
+    },
+    {
+        "id": "key-3",
+        "value": 3
+    },
+    {
+        "id": "key-4",
+        "value": 4
+    },
+    {
+        "id": "key-5",
+        "value": 5
+    },
+    {
+        "id": "key-6",
+        "value": 6
+    },
+    {
+        "id": "key-7",
+        "value": 7
+    },
+    {
+        "id": "key-8",
+        "value": 8
+    },
+    {
+        "id": "key-9",
+        "value": 9
+    },
+    //Operators
+    {
+        "id": "key-divide",
+        "value": "/"
+    },
+    {
+        "id": "key-multiply",
+        "value": "x"
+    },
+    {
+        "id": "key-minus",
+        "value": "-"
+    },
+    {
+        "id": "key-plus",
+        "value": "+"
+    },
+    // {
+    //     "id": "key-equals",
+    //     "value": "="
+    // },
+]
--- javascript/calculator.js
+++ javascript/calculator.js
@@ -1,9 +1,57 @@
+let calculatorArray = [];
+
 const buttonClick = function(e) {
-    console.log("Button is clicked");
-    console.log(e.target.id);
+
+    // Search buttonValues for id
+    const buttonValue = buttonValues.find(item => item.id === e.target.id);
+
+    if (typeof(buttonValue) !== "undefined") {
+        checkInput(buttonValue);  // Check if number or operator
+    }
+
+    if (e.target.id === "key-ac") {
+        clearDisplay();
+    }
+
+    if (e.target.id === "key-equals") {
+        // Evaluate Calculation
+    }
+
+    updateDisplay();
+}
+
+const checkInput = function(buttonValue) {
+    if (typeof(calculatorArray.at(-1)) === "number") {
+        buildNumber(buttonValue.value);
+    } else {
+        appendOperator(buttonValue.value);
+    }
+}
+
+const buildNumber = function(value) {
+    // Get last item in calculator array as string
+    const previousValue = calculatorArray.pop();
+    // append new value to string
+    const newValue = `${previousValue}${value}`;
+    // replace last item in array
+    calculatorArray.push(parseInt(newValue));
+}
+
+const appendOperator = function(value) {
+    calculatorArray.push(value);
+}
+
+const clearDisplay = function() {
+    calculatorArray = [];  // Clear array
+    updateDisplay();  // Update display with cleared array
+};
+
+const updateDisplay = function() {
+    displayCalculation.textContent = calculatorArray.join(" ");
 }
 
 // Select Dom elements
+const displayCalculation = document.querySelector("#display-calculation");
 const buttons = document.querySelectorAll(".button");
 
 // Add event listeners
